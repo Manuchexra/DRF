17f095d3da1d85db3c68922033afc7b038cefe30
unknown <mnurmexrojova@gmail.com>
2024-07-16 21:49:58 +0500

    Html

---
+++ posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import PostListView
+
+app_name='posts'
+urlpatterns=[
+    path('',PostListView.as_view(), name='list')
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.views.generic import ListView, CreateView, UpdateView
 # Create your views here.
+
+class PostListView(ListView):
+    model=Post
+    template_name='list.html'
+    context_object_name='posts'
